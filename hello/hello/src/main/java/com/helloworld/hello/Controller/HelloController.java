439077c68c336690b2504991666ede325d8dc69c
Yuri <yuri.rampazo@outlook.com>
2021-03-16 18:57:57 -0300

    Hello World Spring

---
+++ hello/hello/src/main/java/com/helloworld/hello/Controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.helloworld.hello.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+	
+	@GetMapping
+	public String hello () {
+		return "Hello World \nPersistência\nMentalidade de Crescimento\nProatividade";
+		
+	}
+
+}
